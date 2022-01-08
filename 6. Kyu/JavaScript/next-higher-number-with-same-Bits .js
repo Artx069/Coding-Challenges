36cf2e3b27bc750665e3dd2a03b2cb77268683af
Artx069 <87966606+Artx069@users.noreply.github.com>
2022-01-08 14:51:52 +0100

    08.01.2022

---
+++ 6. Kyu/JavaScript/next-higher-number-with-same-Bits .js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+Your task is to find the next higher number (int) with same '1'- Bits.
+
+I.e. as much 1 bits as before and output next higher than input. Input is always an int in between 1 and 1<<30 (inclusive). No bad cases or special tricks...
+
+Some easy examples:
+Input: 129  => Output: 130 (10000001 => 10000010)
+Input: 127 => Output: 191 (01111111 => 10111111)
+Input: 1 => Output: 2 (01 => 10)
+Input: 323423 => Output: 323439 (1001110111101011111 => 1001110111101101111)
+*/
+function nextHigher(n) {
+  let m = n + 1;
+  while (
+    n
+      .toString(2)
+      .split("")
+      .filter((a) => a == "1").length !=
+    m
+      .toString(2)
+      .split("")
+      .filter((a) => a == "1").length
+  ) {
+    m++;
+  }
+  return m;
+}
