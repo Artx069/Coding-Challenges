50bd3d261a286f6df113ce2be8defc72c7804e13
Artx069 <artx@web.de>
2022-08-15 01:44:20 +0200

    13.08.2022

---
+++ 6. Kyu/Java/Grab.java
@@ -0,0 +1,31 @@
+/*
+Pirates have notorious difficulty with enunciating. They tend to blur all the letters together and scream at people.
+
+At long last, we need a way to unscramble what these pirates are saying.
+
+Write a function that will accept a jumble of letters as well as a dictionary, and output a list of words that the pirate might have meant.
+
+For example:
+
+grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] )
+Should return ["sport", "ports"].
+
+Return matches in the same order as in the dictionary. Return an empty array if there are no matches.
+
+Good luck! 
+*/
+
+import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Grab {
+  
+  public static List<String> grabscrab(String s, List<String> words){
+    return words.stream().filter(x -> sortWord(x).equals(sortWord(s))).collect(Collectors.toList());
+  }
+  
+  public static String sortWord(String s){
+        return String.join("",Arrays.copyOf(Arrays.stream(s.split("")).sorted().map(x -> String.valueOf(x)).toArray(),s.length(), String[].class));
+    }
+}
