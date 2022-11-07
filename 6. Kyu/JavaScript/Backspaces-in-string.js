e74e62e3a8863bcb86ca9e426d0e07960940594f
Artx069 <artx@web.de>
2022-11-07 09:32:52 +0100

    07.11.2022

---
+++ 6. Kyu/JavaScript/Backspaces-in-string.js
@@ -0,0 +1,18 @@
+"use strict";
+/*
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+*/
+function cleanString(s) {
+  while (s.match(/#/)) {
+    s = s.replace(/.#|^#/, "");
+  }
+  return s;
+}
